7aad5e26b1c5c3f5d549875ae1c9f19e5658e5be
MansoorAfzalDurrani <mansoorafzaldurrani@yahoo.com>
2023-11-04 17:31:28 +0000

    Feat: Function romantoDecimal code add and three test cases tested successfully

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { add } from './calc';
+//import { add } from './calc';
 
-console.log(`2 plus 2 is ${add(2, 2)}`);
+//console.log(`2 plus 2 is ${add(2, 2)}`);
+import {romanToDecimal } from './romanToDecimal';
+romanToDecimal('I');
