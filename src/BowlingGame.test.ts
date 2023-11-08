6e5f0c522b39fbeb5da30bbe2c279174213fba18
MansoorAfzalDurrani <mansoorafzaldurrani@yahoo.com>
2023-11-08 08:42:25 +0000

    Factorization Done got Issue to discuss with Tech Coaches in iport statement

--- src/BowlingGame.test.ts
+++ src/BowlingGame.test.ts
@@ -1,16 +1,19 @@
 import { romanToDecimal } from "../src/romanToDecimal";
-const BowlingGame = require('./BowlingGame');
+const BowlingGame= require("./BowlingGame");
+let game = BowlingGame;
+beforeEach(()=>{
+    
+    game= new BowlingGame();
+})
 it('should return 0 for a game of all zero',()=>{
-    const game = new BowlingGame();
-    for(let i=0; i<20;i++){
-        game.roll(0);
-    }
-    expect(game.score).toBe(0);
+     expect(game.score).toBe(0);
 });
     it('should return 20 for a game of all one', ()=>{
-        const game = new BowlingGame();
-        for (let i=0; i<20; i++){
-            game.roll(1);
-        }
+        rollMany(1,20);
         expect(game.score).toBe(20);
     });
+    function rollMany(pins: number,rolls:number){
+        for (let i=0; i<rolls; i++){
+            game.roll(pins);
+        }
+    }
