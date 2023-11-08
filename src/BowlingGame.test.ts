4d44bf678abe0f3e19a84fffa8ef9a42172ace81
MansoorAfzalDurrani <mansoorafzaldurrani@yahoo.com>
2023-11-08 12:39:47 +0000

    test case for spare added and tested successfull will see wrong import state once finished all test

--- src/BowlingGame.test.ts
+++ src/BowlingGame.test.ts
@@ -2,18 +2,30 @@ import { romanToDecimal } from "../src/romanToDecimal";
 const BowlingGame= require("./BowlingGame");
 let game = BowlingGame;
 beforeEach(()=>{
-    
-    game= new BowlingGame();
+  game= new BowlingGame();
 })
 it('should return 0 for a game of all zero',()=>{
+     rollMany(0,20);
      expect(game.score).toBe(0);
 });
     it('should return 20 for a game of all one', ()=>{
         rollMany(1,20);
         expect(game.score).toBe(20);
     });
+    it('should return the correct score when a spare is rolled', ()=>{
+        game.roll(5);
+        game.roll(5);
+        game.roll(3)
+        rollMany(0,17);
+        expect(game.score).toBe(16);
+    });
+
+        
     function rollMany(pins: number,rolls:number){
         for (let i=0; i<rolls; i++){
             game.roll(pins);
         }
+       
     }
+        
+    
--- src/BowlingGame.ts
+++ src/BowlingGame.ts
@@ -1,13 +1,31 @@
-class BowlingGame{
-    private runningScore: number;
-    constructor(){
-     this.runningScore=0;
+ 
+  export class BowlingGame{
+
+    private rolls: number[];
+
+     constructor(){
+     this.rolls =[];
     }
- roll(pins:number){
-  this.runningScore += pins;
+     
+    roll(pins: number){
+        this.rolls.push(pins);
+    }
+  get score(){
+    let score = 0;
+    let rollIndex =0;
+    for (let frameIndex=0; frameIndex<10; frameIndex++){
+        const framScore =this.rolls[rollIndex] + this.rolls[rollIndex+1];
+        if(framScore === 10){
+         score +=10 + this.rolls[rollIndex+2];
+         rollIndex +=2;
+        }else{
+        score += framScore ;
+        rollIndex += 2;
+        }
+        
+        }   
+        return score;
+    }
  }
- get score(){
-    return this.runningScore;
- }
-}
-module.exports=BowlingGame;
+ module.exports=BowlingGame;
+  
